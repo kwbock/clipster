868a19d4478feb1036db9530614525c3435b793c
Daniel <dswhite21@gmail.com>
2012-10-04 12:45:39 -0400

    Added ID creation for a Clip
    ID creation is based on seconds and fractional seconds since epoch
    translated to base 36

--- app/models/clipster/clip.rb
+++ app/models/clipster/clip.rb
@@ -1,5 +1,11 @@
 module Clipster
   class Clip < ActiveRecord::Base
+    before_create :init_id
     attr_accessible :clip, :id, :language
+    
+    private
+      def init_id
+        self.id = Time.now.to_f.to_s.gsub('.','').to_i.to_s(36)
+      end
   end
 end
