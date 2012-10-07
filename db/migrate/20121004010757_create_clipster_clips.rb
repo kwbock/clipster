b1abe39bc3de9cb7026f1bf8f41a21966196b831
Kyle Bock <kylewbock@gmail.com>
2012-10-07 12:20:29 -0400

    addding proper migrations, this most likely broke validations

--- db/migrate/20121004010757_create_clipster_clips.rb
+++ db/migrate/20121004010757_create_clipster_clips.rb
@@ -1,12 +1,16 @@
 class CreateClipsterClips < ActiveRecord::Migration
-  def change
-    create_table :clipster_clips, :id => false do |t|
-      t.string :id, :options => 'PRIMARY KEY'
+  def up
+    create_table :clipster_clips do |t|
+      t.string :hash, :options, :null => false
       t.text :clip, :null => false
       t.string :language, :null => false
       t.string :title, :null => false
       t.boolean :private, :null => false
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :clipster_clips
   end
 end
