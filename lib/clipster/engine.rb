5dadc924bdb06fff50181008e944aea6785f4ca3
Kyle Bock <kylewbock@gmail.com>
2012-10-15 08:59:48 -0400

    initial rspec setup prior to removing test

--- lib/clipster/engine.rb
+++ lib/clipster/engine.rb
@@ -4,5 +4,11 @@ require 'dynamic_form'
 module Clipster
   class Engine < ::Rails::Engine
     isolate_namespace Clipster
+
+    # so we can use rspec generators
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
   end
 end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '3a00284e27e1f74d775195af19466e064669b1684d336e75939c1e060ca60e327455b1f3b05bb9ab5a94dad63901cde72c1a035d48c902cfdc70abb162780d51'
