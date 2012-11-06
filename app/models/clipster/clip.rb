a8074c094d69092c3622260c0bf0bf281f6ca771
Daniel <dswhite21@gmail.com>
2012-11-06 16:11:58 -0500

    Shows expiring time in both clip view and list view.

--- app/models/clipster/clip.rb
+++ app/models/clipster/clip.rb
@@ -1,31 +1,36 @@
 module Clipster
   class Clip < ActiveRecord::Base
+    include ActionView::Helpers::DateHelper
     before_create :init_id
     self.primary_key = :url_hash
-    attr_accessible :clip, :language, :title, :private, :expires
-    attr_accessor :lifespan
+    attr_accessible :clip, :language, :title, :private, :expires, :lifespan
     
     # scope utilized by search functionality.
     # TODO: build more powerful search term creation
     scope :search, lambda {|term| 
-      where("title LIKE :term or language LIKE :term or clip LIKE :term",{
-          :term => term
+      where("(title LIKE :term or language LIKE :term or clip LIKE :term) and (expires is null OR expires < :now)",{
+          :term => term,
+          :now => DateTime.now
       })
     }
     
     validates :clip, :length => {:minimum   => 3}
     validates :title, :length => {:minimum   => 1}
     
-    def expires=(lifespan)
+    def lifespan=(lifespan)
       case lifespan
-      when "An Hour"
-        self.expires = DateTime.advance(:hours=>1)
-      when "A Day"
-        self.expires = DateTime.advance(:days=>1)
-     when "A Month"
-        self.expires = DateTime.advance(:months=>1)
-      when "A Year"
-        self.expires = DateTime.advance(:years=>1)
+        when "An Hour" then self.expires = DateTime.now.advance(:hours=>1)
+        when "A Day"   then self.expires = DateTime.now.advance(:days=>1)
+        when "A Month" then self.expires = DateTime.now.advance(:months=>1)
+        when "A Year"  then self.expires = DateTime.now.advance(:years=>1)
+      end
+    end
+    
+    def lifespan
+      unless self.expires.nil?
+        time_ago_in_words(self.expires)
+      else
+        "the end of time"
       end
     end
     
