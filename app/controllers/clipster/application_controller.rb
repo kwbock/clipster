2416f14afcb9b70173defc9e1a2dcd88fe74abe6
Kyle Bock <kylewbock@gmail.com>
2012-11-14 15:04:01 -0500

    removed application index action and removed erroneous equal sign from set_primary_key

--- app/controllers/clipster/application_controller.rb
+++ app/controllers/clipster/application_controller.rb
@@ -2,10 +2,6 @@ module Clipster
   class ApplicationController < ActionController::Base
     before_filter :set_current_user
 
-    def index
-      render :text => "Welcome 2 Clipster"
-    end
-
     private
       def set_current_user
         begin
